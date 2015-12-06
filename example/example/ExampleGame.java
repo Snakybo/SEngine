e8cefef6781a95c7aa67a81a5cbdb5385b4163af
Kevin Krol <Krol.kevin@gmail.com>
2015-12-06 21:26:28 +0100

    Disabled example code

--- example/example/ExampleGame.java
+++ example/example/ExampleGame.java
@@ -20,109 +20,114 @@ import com.snakybo.sengine.utils.math.Quaternion;
 import com.snakybo.sengine.utils.math.Vector3f;
 
 // Your core game class should extend the Game class
-public class ExampleGame extends Game {
-	
+public class ExampleGame extends Game
+{
+/*
 	// Called whenever the engine has been started
 	@Override
-	public void init(Window window) {
-		// Create a matrix holding information about the projection of the camera, parameters are: FOV, aspect ratio, Z-Near and Z-Far
-		Matrix4f cameraProjection = new Matrix4f().initPerspective(90, (float)Window.getWidth() / (float)Window.getHeight(), 0.01f, 1000);
-		
+	public void init(Window window)
+	{
+		// Create a matrix holding information about the projection of the
+		// camera, parameters are: FOV, aspect ratio, Z-Near and Z-Far
+		Matrix4f cameraProjection = new Matrix4f().initPerspective(90,
+				(float) Window.getWidth() / (float) Window.getHeight(), 0.01f, 1000);
+
 		// Create a new camera game-object
 		GameObject camera = new GameObject();
-		
+
 		// Add a free-look component to the camera game-object
 		camera.addComponent(new FreeLook(Window.getCenter()));
-		
+
 		// Add a free-move component to the camera game-object
 		camera.addComponent(new FreeMove());
-		
-		// Add the camera-component to the camera game-object, passing in the camera projection to the component
+
+		// Add the camera-component to the camera game-object, passing in the
+		// camera projection to the component
 		camera.addComponent(new CameraComponent(cameraProjection));
-		
-		// Create a set of materials, there's no need to assign them to a variable, they're only loaded once
+
+		// Create a set of materials, there's no need to assign them to a
+		// variable, they're only loaded once
 		new Material(
 				// The name of the material
 				"bricks",
-				
+
 				// The diffuse texture of the material
 				new Texture("example/bricks.png"),
-				
+
 				// The specular intensity of the material
 				0.5f,
-				
+
 				// The specular exponent of the material
 				4,
-				
+
 				// The normal-map of the material
 				new Texture("example/bricks_normal.png"),
-				
+
 				// The displacement map of the material
 				new Texture("example/bricks_disp.png"),
-				
+
 				// The displacement map scale
 				0.03f,
-				
+
 				// The displacement map offset
-				-0.5f
-			);
-		
-		
-		new Material(
-				"bull",
-				new Texture("example/bull_texture.jpg"),
-				4,
-				8,
-				new Texture("example/bull_normal.jpg")
-			);
-		
-		// Create a mesh renderer for the bull game-object, note that we create a material with the name "bull", this will cause it to use the previously created material
+				-0.5f);
+
+		new Material("bull", new Texture("example/bull_texture.jpg"), 4, 8, new Texture("example/bull_normal.jpg"));
+
+		// Create a mesh renderer for the bull game-object, note that we create
+		// a material with the name "bull", this will cause it to use the
+		// previously created material
 		MeshRenderer bullMesh = new MeshRenderer(new Mesh("example/bull.obj"), new Material("bull"));
-		
-		// When creating game-objects you can pass in the position, rotation and scale directly in the game object
+
+		// When creating game-objects you can pass in the position, rotation and
+		// scale directly in the game object
 		GameObject bullObject = new GameObject(new Vector3f(2, -0.4f, 2), new Quaternion(), 5);
-		
+
 		// Add the mesh renderer component to the bull game-object
 		bullObject.addComponent(bullMesh);
-		
+
 		// Add the camera-and bull game-objects to the scene
 		addChild(camera);
 		addChild(bullObject);
-		
-		// You can also create game-objects on one line, and adding them immediately
-		addChild(
-				new GameObject(new Vector3f(0, -1, 0))
-				.addComponent(new MeshRenderer(new Mesh("internal/plane.obj"), new Material("bricks")))
-			);
-		
+
+		// You can also create game-objects on one line, and adding them
+		// immediately
+		addChild(new GameObject(new Vector3f(0, -1, 0))
+				.addComponent(new MeshRenderer(new Mesh("internal/plane.obj"), new Material("bricks"))));
+
 		// Add lighting to the scene
 		addLights();
 	}
-	
-	private void addLights() {
-		// Create a new directional light, with a light-gray color and an intensity of 0.1
+
+	private void addLights()
+	{
+		// Create a new directional light, with a light-gray color and an
+		// intensity of 0.1
 		DirectionalLight directionalLight = new DirectionalLight(new Color(0.8f, 0.8f, 0.8f), 0.1f);
-		
-		// Create a new point light, with a cyan color, an intensity of 0.9 and an attenuation-exponent value of 1
+
+		// Create a new point light, with a cyan color, an intensity of 0.9 and
+		// an attenuation-exponent value of 1
 		PointLight pointLight = new PointLight(new Color(0, 1, 0.5f), 0.9f, new Attenuation(0, 0, 1));
-		
-		// Create a new spot light, with a yellow color, a full intensity of 1, an attenuation-exponent value of 0.05 and a cutoff of 0.7
+
+		// Create a new spot light, with a yellow color, a full intensity of 1,
+		// an attenuation-exponent value of 0.05 and a cutoff of 0.7
 		SpotLight spotLight = new SpotLight(new Color(1, 1, 0), 1, new Attenuation(0, 0, 0.05f), 0.7f);
-		
+
 		// Create game-objects for each light
 		GameObject directionalLightObject = new GameObject().addComponent(directionalLight);
 		GameObject pointLightObject = new GameObject().addComponent(pointLight);
 		GameObject spotLightObject = new GameObject().addComponent(spotLight);
-		
+
 		// Transform the light objects
-		directionalLightObject.getTransform().setRotation(new Quaternion(new Vector3f(1, 0, 0), (float)Math.toRadians(-90)));
-		
+		directionalLightObject.getTransform()
+				.setRotation(new Quaternion(new Vector3f(1, 0, 0), (float) Math.toRadians(-90)));
+
 		spotLightObject.getTransform().getLocalPosition().set(3, 0, 3);
-		spotLightObject.getTransform().setRotation(new Quaternion(new Vector3f(0, 1, 0), (float)Math.toRadians(90)));
-		
+		spotLightObject.getTransform().setRotation(new Quaternion(new Vector3f(0, 1, 0), (float) Math.toRadians(90)));
+
 		// Add the lights to the scene
 		addChild(directionalLightObject);
 		addChild(pointLightObject);
 		addChild(spotLightObject);
-	}
+	}*/
 }
--- example/example/Main.java
+++ example/example/Main.java
@@ -4,27 +4,30 @@ import com.snakybo.sengine.core.SEngine;
 import com.snakybo.sengine.rendering.Window;
 import com.snakybo.sengine.utils.math.Vector3f;
 
-public class Main {
-	public static void main(String[] args) {
-		// Create a new instance of the engine, passing in your core game class
+public class Main
+{
+	public static void main(String[] args)
+	{
+		/*// Create a new instance of the engine, passing in your core game class
 		SEngine engine = new SEngine(new ExampleGame());
-		
-		// Create a new window, passing in the width, height and title of the window
+
+		// Create a new window, passing in the width, height and title of the
+		// window
 		Window window = new Window(1280, 720, "Example game");
-		
+
 		// Set the ambient color to a dark gray
 		window.setAmbientColor(new Vector3f(0.2f, 0.2f, 0.2f));
-		
+
 		// Set the clear color to purple
 		window.setClearColor(new Vector3f(0.5f, 0.0f, 1.0f));
-		
+
 		// Set the desired level of AA, 16 in this case.
 		window.setAA(Window.MSAA, 16);
-		
+
 		// Create the window
 		window.create();
-		
+
 		// Start the engine, passing in the window and the desired framerate
-		engine.start(window, 60.0);
+		engine.start(window, 60.0);*/
 	}
 }
