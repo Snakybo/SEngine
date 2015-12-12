ce03fff77bf8ba3adfc3a1ffb759cbcdd42e4f75
Kevin Krol <Krol.kevin@gmail.com>
2015-12-12 19:40:20 +0100

    Cleaned up MathUtils

--- src/com/snakybo/sengine/utils/math/MathUtils.java
+++ src/com/snakybo/sengine/utils/math/MathUtils.java
@@ -1,22 +1,22 @@
 package com.snakybo.sengine.utils.math;
 
-/** @author Kevin
- * @since Jul 12, 2014 */
+/** 
+ * @author Kevin
+ * @since Jul 12, 2014
+ */
 public class MathUtils
 {
 	public static <T extends Comparable<T>> T clamp(T val, T min, T max)
 	{
-		if (val.compareTo(min) < 0)
+		if(val.compareTo(min) < 0)
 		{
 			return min;
 		}
-		else if (val.compareTo(max) > 0)
+		else if(val.compareTo(max) > 0)
 		{
 			return max;
 		}
-		else
-		{
-			return val;
-		}
+		
+		return val;
 	}
 }
